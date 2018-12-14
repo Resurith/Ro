c73d59525c6802f3da58090747036de499b074fb
Resurith <45875455+Resurith@users.noreply.github.com>
2018-12-14 13:55:16 +0100

    Update Code666.py

--- Code666.py
+++ Code666.py
@@ -1,75 +1,73 @@
-from math import exp, cos
-Salut RO
+rom math import *
 from numpy import *
 from scipy import *
 
-sigma = 5.67036713 * 10 ** (-8)
-chaleur_latente_eau = 2257
-flux_direct_test = 900
-capacite_calo_air = 1000
-enthalpie_vap_eau = 2264.76
-pression_atmo = 101325
-R = 8.31
-temp_evap = 373.15
-masse_moleculaire_eau = 18.01528
+sigma = 5.67036713 * 10 ** (-8)  # Kg.m²/s³.m³.K^4
+chaleur_latente_eau = 2257  # KJ/Kg
+flux_direct_test = 900  # w/m²
+capacite_calo_air = 1000  # J/Kg K
+p0 = 101325  # Pa
+R = 8.31  # J/mol.K #cste stephan-Boltzmann
+t0 = 373.15  # K
+masse_moleculaire_eau = 18.01528  # g/mol
 alpha = 0
 
 """Module environement """
 
 
-def pression_saturation(temperature_ambiante):
-    p_sat = pression_atmo * exp(-enthalpie_vap_eau / R * (1 / temperature_ambiante - 1 / temp_evap))
-    return p_sat
+def env_humidite_abs(temperature_ambiante):
+    p_sat = p0 * exp(-chaleur_latente_eau / R * (1 / temperature_ambiante - 1 / t0))
+    print("pression de saturation :", p_sat, "Pa")  # Debug
+
+    Y_abs = (p_sat * masse_moleculaire_eau) / (temperature_ambiante * R)
+    print("Humidité absolue :", Y_abs, "g(H2O)/m³")  # Debug
+
+    return p_sat, Y_abs
 
 
-def humidite_absolue(temperature_ambiante):
-    Y_abs = (pression_saturation(temperaure_ambiante) * masse_moleculaire_eau) / temperature_ambiante * R
-    return Y_abs
-
-
-def rayonnement_indirect(humidite_relative_ambiante, temperature_ambiante):
+def rayonnements(humidite_relative_ambiante, temperature_ambiante):
     temperature_rosee = ((humidite_relative_ambiante * 100) ** (1 / 8)) * (
-            112 + 0.9 * temperature_ambiante) + 0.1 * temperature_ambiante - 112
+                112 + 0.9 * temperature_ambiante) + 0.1 * temperature_ambiante - 112
+    print("Température de rosée : ", temperature_rosee, "°C")  # Debug #°C
+
     temperature_ciel = temperature_ambiante * (
-            (0.711 + 0.0056 * temperature_rosee + 7.3 * temperature_rosee ** 2 * 10 ** (-5)) ** (1 / 4))
-    rayonnements_i = sigma * (temperature_ciel ** 4)
-    return rayonnements_i
+                (0.711 + 0.0056 * temperature_rosee + 7.3 * temperature_rosee ** 2 * 10 ** (-5)) ** (1 / 4))
+    print("T sky : ", temperature_ciel, "°C")  # Debug #°C
 
+    Fi = sigma * (temperature_ciel ** 4)
+    print("Rayonnements indirects :", Fi, "W/m²")
 
-def rayonnement_direct():
     Fd = (R - rayonnement_indirect(humidité_relative_ambiante, temperaure_ambiante)) / cos(alpha)
-    return Fd
+    print("Rayonnements directs :", Fd, "W/m²")
+
+    return Fi, Fd
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 """Module Ventilation"""
 
 
-def vitesse_evaporation(kg_de_matiere_a_secher, humidite_initial_du_produit, humidite_final_du_produit,
-                        temps_de_sechage):
+def ventilation(kg_de_matiere_a_secher, humidite_initial_du_produit, humidite_final_du_produit, temps_de_sechage):
     vitesse_evaporation_h2o = (humidite_initial_du_produit - humidite_final_du_produit) * kg_de_matiere_a_secher / (
-            1 + humidite_initial_du_produit) * 1 / temps_de_sechage
-    return vitesse_evaporation_h2o
+                1 + humidite_initial_du_produit) * 1 / temps_de_sechage
+    print("vitesse d'évaporation :", vitesse_evaporation_h2o, "Kg / s") #Debug
 
-
-def ventilation(humidite_relative_ambiante, humidite_relative_finale):
-    debit = vitesse_evaporation(kg_de_matiere_à_sécher, humidité_initiale_du_produit, humidité_finale_du_produit,
-                                temps_de_séchage) / (humidite_relative_finale - humidite_relative_ambiante)
+    debit = vitesse_evaporation_h2o / (humidite_relative_finale - humidite_relative_ambiante)
+    print("Débit :", debit, "Kg(air sec) / s") #Debug
     return debit
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 """Module Effet de serre"""
 
+
 def puissance():
-    puissance_total = ventilation(humidité_relative_ambiante, humidité_relative_finale) * capacite_calo_air * (
-            T - temperaure_ambiante) + chaleur_latente_eau * vitesse_evaporation(kg_de_matiere_à_sécher,
-                                                                                 humidité_initiale_du_produit,
-                                                                                 humidité_finale_du_produit,
-                                                                                 temps_de_séchage)
+    puissance_total = ventilation(humidité_relative_ambiante, humidité_relative_finale) * capacite_calo_air * \
+                      (T - temperaure_ambiante) + chaleur_latente_eau * \
+                      vitesse_evaporation(kg_de_matiere_à_sécher, humidité_initiale_du_produit,
+                                          humidité_finale_du_produit, temps_de_séchage)
     return puissance_total
-
-
-
-
 def effet_de_serre(z):
     k = 10
     T_amb = 65
@@ -99,6 +97,7 @@ zGuess = [100, 30, 30, 50, 60]
 z = fsolve(effet_de_serre, zGuess)
 print(effet_de_serre([0, 0, 0, 0, 0]))
 
+
 def convection():
     mu = 1.8 * 10 ** (-5)  # viscosité dynamique
     rho = 1.225  # masse volumique varie en fct de la Temp
@@ -116,9 +115,8 @@ def convection():
     Nu = (Nu_naturelle ** 3 + Nu_interne ** 3) ** (1 / 3)
     k_fluid = 101000  # pa de l'air module d'elasticité isostatique
     h = Nu * k_fluid / L
+
     return h
-
-
 """Code Principal"""
 
 kg_de_matiere_à_sécher = float(input("Kg de matière à sécher (Kg):  "))
